66e8e56927af1fcee6401a850dbb3cc2211b51d0
joaovitorscr <contato.joaovitorscr@gmail.com>
2023-12-05 13:24:28 -0300

    Render message when user has no boxes inside a list

--- app/box/[id]/BoxPage.tsx
+++ app/box/[id]/BoxPage.tsx
@@ -19,7 +19,6 @@ import * as z from 'zod'
 import { toast } from '../../../components/ui/use-toast'
 import { Plus, Trash } from 'lucide-react'
 import Container from '@/components/Container'
-import { Skeleton } from '@/components/ui/skeleton'
 import { useSession } from 'next-auth/react'
 import Loading from './loading'
 
@@ -161,18 +160,26 @@ export default function BoxPage({ boxUrl }: BoxPageInterface) {
         </div>
       </div>
       <div className="space-y-4 mt-40 justify-center">
-        {box?.content.map((item) => (
-          <div className="flex items-center justify-center" key={item.id}>
-            <Checkbox
-              id={item.id}
-              checked={item.checked}
-              content={item.content}
-            />
-            <Button variant={'ghost'} onClick={() => deleteItem(item.id)}>
-              <Trash />
-            </Button>
+        {box?.content.length! > 0 ? (
+          <div>
+            {box?.content.map((item) => (
+              <div className="flex items-center justify-center" key={item.id}>
+                <Checkbox
+                  id={item.id}
+                  checked={item.checked}
+                  content={item.content}
+                />
+                <Button variant={'ghost'} onClick={() => deleteItem(item.id)}>
+                  <Trash />
+                </Button>
+              </div>
+            ))}
           </div>
-        ))}
+        ) : (
+          <div className="flex items-center justify-center">
+            <h3 className="text-2xl">Start creating your first box!</h3>
+          </div>
+        )}
       </div>
     </Container>
   )
